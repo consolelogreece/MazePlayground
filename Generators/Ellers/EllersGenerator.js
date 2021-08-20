3d028a8a313b5c28652ab388a7e93d2c8016e9f0
consolelogreece <consolelogreece@gmail.com>
2021-08-20 10:47:39 +0100

    started creation of ellers algorithm maze generator

---
+++ Generators/Ellers/EllersGenerator.js
@@ -0,0 +1,22 @@
+class EllersMazeGen
+{
+    constructor(mazeHeight, mazeWidth, startCellCoords, endCellCoords, completedCallback){
+        this.mazeWidth = mazeWidth;
+        this.mazeHeight = mazeHeight;
+        this.startCellCoords = startCellCoords;
+        this.endCellCoords = endCellCoords;
+        this.maze = [];
+        this.completed = false;
+        this.completedCallback = completedCallback;
+    }
+
+    StepMaze()
+    { 
+       
+    }
+
+    Draw(illustrator)
+    {   
+        
+    }
+}
