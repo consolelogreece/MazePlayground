bedf97178366c79bdc5d8509bd022058ed3091d7
consolelogreece <consolelogreece@gmail.com>
2021-08-24 11:12:59 +0100

    added working tremaux

---
+++ Solvers/Tremaux.js
@@ -0,0 +1,234 @@
+class TremauxMazeSolver 
+{
+    constructor(maze, startCellCoords, endCellCoords)
+    {
+        this.startCellCoords = startCellCoords;
+        this.endCellCoords = endCellCoords;
+        this.completed =  false;
+        this.initialDraw = true;
+        this.cellsToDraw = [];
+        this.path = [];
+        this.currentPath = [];
+
+        let formattedMaze = [];
+
+        for (let row = 0; row < maze.length; row++)
+        {
+            let formattedRow = [];
+            for (let col = 0; col < maze[row].length; col++)
+            {
+                formattedRow.push({...maze[row][col], marks: 0, isCurrent: false, visited:false, junction: maze[row][col].connectedCells.length > 2, drawConCellList: []});
+            }
+
+            formattedMaze.push(formattedRow);
+        }
+
+        this.maze = formattedMaze;
+
+        this.maze[this.startCellCoords.row][this.startCellCoords.col].isCurrent = true;
+    }
+
+    /*
+    1 Every time you visit a cell, mark it once.
+    2 When you hit a dead end, turn around and go back.
+    3 When you hit a junction you haven't visited, pick a new passage at random.
+    4 If you're walking down a new passage and hit a junction you have visited, treat it like a dead end and go back.
+    5 If walking down a passage you have visited before (i.e. marked once) and you hit a junction, take any new passage available, otherwise take an old passage (i.e. marked once).
+    6 When you finally reach the end, follow cells marked exactly once back to the start.
+    7 If the Maze has no solution, you'll find yourself at the start with all cells marked twice.
+    */
+
+    IsJunction(cell) 
+    {
+        
+        // 3 When you hit a junction you haven't visited, pick a new passage at random.
+        // 4 If you're walking down a new passage and hit a junction you have visited, treat it like a dead end and go back.
+        // 5 If walking down a passage you have visited before (i.e. marked once) and you hit a junction, take any new passage available, otherwise take an old passage (i.e. marked once).
+    }
+
+    IsDeadEnd(cell)
+    {
+
+    }
+
+    * StepMaze()
+    { 
+        let endCell = this.maze[this.endCellCoords.row][this.endCellCoords.col];
+
+        let previousCell = this.maze[this.startCellCoords.row][this.startCellCoords.col];
+
+        previousCell.visited = true;
+
+        previousCell.marks++;
+
+        let connectedNeighbours = FindNeighbours(this.maze, previousCell.row, previousCell.col).filter(neighbour => previousCell.connectedCells.includes(neighbour.dir));
+
+        let currentCell = connectedNeighbours[0].cell;  
+
+        yield this;
+
+        while (!this.completed)
+        {   
+            if (currentCell == endCell) {this.completed = true; currentCell.marks++; continue;}
+            let nextCell;
+
+            let connectedNeighbours = FindNeighbours(this.maze, currentCell.row, currentCell.col).filter(neighbour => currentCell.connectedCells.includes(neighbour.dir));
+            
+            //deadend
+            if (connectedNeighbours.length === 1)
+            {
+                currentCell.marks++;
+                nextCell = previousCell;
+                previousCell = currentCell;
+            }
+            // on path, only 1 way to go
+            else if(connectedNeighbours.length === 2)
+            {                                  
+                nextCell = connectedNeighbours.filter(neighbour => neighbour.cell !== previousCell)[0].cell;
+                previousCell = currentCell;    
+            }
+            // junction, baby!
+            else 
+            {
+                // first time at junction, pick new at random
+                if (currentCell.marks === 0)
+                {
+                    previousCell = currentCell;
+                    let validNeighbours = connectedNeighbours.filter(neighbour => neighbour.cell !== currentCell && neighbour.cell.marks === 0);
+                    nextCell = validNeighbours[0].cell;
+                }
+                // been at junction before and not been on this path before, treat like dead end
+                else if (previousCell.marks === 1)
+                {
+                    let tempCell = currentCell;
+                    currentCell = previousCell;
+                    previousCell = tempCell;
+                    nextCell = currentCell;
+                    
+                    currentCell.marks++;
+                }
+                else
+                {
+                    let validNeighbours = connectedNeighbours.filter(neighbour => neighbour.cell !== currentCell);
+                    let unvisitedNeighbours = validNeighbours.filter(neighbour => neighbour.cell.marks === 0);
+                    let visitedOnce = validNeighbours.filter(neighbour => neighbour.cell.marks === 1);
+                    if (unvisitedNeighbours.length !== 0)
+                    {                        
+                        previousCell = currentCell;
+                        nextCell = unvisitedNeighbours[0].cell;
+                    }
+                    else 
+                    {                       
+                        previousCell = currentCell;
+                        nextCell = visitedOnce[0].cell;
+                    }
+
+                    if (nextCell.marks == 0) currentCell.marks--;
+                }
+            }
+
+            currentCell.marks++;
+
+            currentCell = nextCell;
+
+            previousCell.isCurrent = false;
+
+            currentCell.isCurrent = true;
+
+            previousCell.drawConCellList.push(nextCell);
+
+            this.cellsToDraw.push(previousCell, nextCell)
+
+            yield this;
+        };       
+
+        this.MapPath();
+        
+        yield this;
+    }
+
+    MapPath()
+    {
+        let startCell = this.maze[this.startCellCoords.row][this.startCellCoords.col];
+
+        let currentCell = startCell;
+
+        let previousCell = startCell
+
+        let endCell = this.maze[this.endCellCoords.row][this.endCellCoords.col];
+
+        while (currentCell != endCell)
+        {
+            this.path.push(currentCell);
+
+            let connectedNeighbours = FindNeighbours(this.maze, currentCell.row, currentCell.col).filter(neighbour => currentCell.connectedCells.includes(neighbour.dir));
+
+            // Find the neighbour which isnt the current cell and has only been marked once, should only be 1.
+            let nextCell = connectedNeighbours.filter(neighbour => neighbour.cell != previousCell && neighbour.cell.marks === 1)[0].cell;
+
+            previousCell = currentCell;
+
+            currentCell = nextCell;
+        }
+
+        // Finally, add end cell
+        this.path.push(endCell);
+    }
+
+    Draw(illustrator)
+    {
+        if (this.initialDraw || this.completed)
+        {
+            illustrator.DrawGrid();
+
+            illustrator.DrawCircleAtLocation(this.startCellCoords.row, this.startCellCoords.col, (dimensions) => dimensions.width / 1.3, "red");
+            illustrator.DrawCircleAtLocation(this.endCellCoords.row, this.endCellCoords.col, (dimensions) => dimensions.width / 1.3, "red");
+            
+            for (let row = 0; row < this.maze.length; row++)
+            {
+                for (let col = 0; col < this.maze[row].length; col++)
+                {
+                    illustrator.DrawWallBreaks(this.maze[row][col]);
+                }
+            }
+
+           this.initialDraw = false;
+        }   
+
+        this.cellsToDraw.forEach(cellFrom => {
+
+            cellFrom.drawConCellList.forEach(cellTo => {
+                // means they are vertical neighbours
+                if (cellFrom.row != cellTo.row)
+                {
+                    let offset = 1;
+                    if (cellFrom.row > cellTo.row) offset = -1;
+                    illustrator.DrawLineBetweenCells(cellFrom.row, cellFrom.col, cellTo.row, cellTo.col, () => 0,  dimensions => (dimensions.height / 3) * offset,  "orange");
+                }
+                else
+                {
+                    let offset = 1;
+                    if (cellFrom.col > cellTo.col) offset = -1;
+                    illustrator.DrawLineBetweenCells(cellFrom.row, cellFrom.col, cellTo.row, cellTo.col, dimensions => (dimensions.width / 3) * offset, () => 0,  "orange");
+                }
+                
+            });
+            
+            if (cell.isCurrent) illustrator.DrawCircleAtLocation(cell.row, cell.col, (dimensions) => dimensions.width / 1.3, "cyan");
+
+            // if (cell.junction || true) illustrator.DrawTextInCell(cell.row, cell.col, cell.marks)
+        });
+
+        if (this.completed)
+        {
+            for (let i = 0; i < this.path.length - 1; i++)
+            {
+                let from = this.path[i];
+                let to = this.path[i + 1];
+                illustrator.DrawLineBetweenCells(from.row, from.col, to.row, to.col, "magenta");
+            }
+        }       
+
+        this.cellsToDraw = [];
+    }
+}
